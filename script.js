7a29763246b82a8243f84095547a0295eb428559
Wallison-dos-Santos <wallison.santos@sagatechbrasil.com.br>
2025-02-23 22:25:13 -0300

    Adiciona página de portfólio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Adiciona classe 'scrolled' ao header quando rolar a página
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
